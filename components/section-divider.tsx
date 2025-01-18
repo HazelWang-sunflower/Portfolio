6697d4b9a44434fe79b9269473b92173d31dae95
Hazel <wyh_sunflower@163.com>
2025-01-18 14:10:23 +0800

    fix motion issue

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -6,7 +6,7 @@ export default function SectionDivider() {
         <motion.div
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            delay={0.125}
+            transition={{ delay: 0.175 }}
             className="my-24 hidden h-16 w-1 rounded-full bg-gray-200 sm:block"
         ></motion.div>
     );
